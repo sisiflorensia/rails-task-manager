519fe80639638993fe92b66630b535a473791e21
sisi florensia <sisiflorensia@gmail.com>
2019-10-22 16:03:51 +0800

    Check how to fix form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,53 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # READ All
+  def index
+    @tasks = Task.all
+  end
+
+  # CREATE step 1
+  def new
+    @task = Task.new
+  end
+
+  # CREATE step 2
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  # READ one
+  def show
+    # set_task
+  end
+
+  # UPDATE step 1
+  def edit
+    # set_task
+  end
+
+  # UPDATE step 2
+  def update
+    #set_task
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  # DELETE
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+
+  # # These are routes for Task model CRUD actions
+  # get "/tasks", to: "tasks#index", as: :tasks       # READ all
+
+  # get "/tasks/new", to: "tasks#new", as: :new_task  # CREATE 1st step: gather info through a form
+  # post "/tasks", to: "tasks#create"                 # CREATE 2nd step: add to Active Records
+
+  # get "/tasks/:id", to: "tasks#show", as: :task     # READ one
+
+  # get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task   # UPDATE 1st step: gather info through a form
+  # patch "/tasks/:id", to: "tasks#update"                     # UPDATE 2nd step: update in Active Records
+
+  # delete "/tasks/:id", to: "tasks#destroy"    # DELETE a task from Active Records
 end
